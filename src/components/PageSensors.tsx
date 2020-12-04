bbc4257a85fadf40568b48c76e65e00513b51fce
Oshadha Gunasekara <oshadha123@gmail.com>
2020-12-03 21:07:13 -0600

    add roslib integration

--- src/components/PageSensors.tsx
+++ src/components/PageSensors.tsx
@@ -1,7 +1,65 @@
+import { resolveSoa } from 'dns';
 import React from 'react';
 
-export default class Sensors extends React.Component {
+var roslib = require('roslib');
+
+// constants
+const WS_URL = 'ws://192.168.0.26:8080';
+
+interface SensorsProps {
+
+}
+
+interface SensorsState {
+  ros: any,
+  value: string
+}
+
+export default class Sensors extends React.Component<SensorsProps,SensorsState> {
+  constructor(props:SensorsProps) {
+    super(props);
+
+    // setup state variables
+    this.state = {
+      ros: new roslib.Ros(),
+      value: ""
+    };
+
+    // bind handlers
+    this.chatterCallback = this.chatterCallback.bind(this);
+  }
+
+  componentDidMount() {
+    this.initializeROS();
+    this.initializePubsSubs(); 
+  }
+
+  initializeROS() {
+    this.state.ros.on('error', (error: any) => console.log(error));
+    this.state.ros.on('connection', (error: any) => console.log('Connection made!'));
+    this.state.ros.on('close', (error: any) => console.log('Connection closed.'));
+    this.state.ros.connect(WS_URL);
+  }
+
+  initializePubsSubs() {
+    // chatter sub
+    var chatterSub = new roslib.Topic({
+      ros: this.state.ros,
+      name: '/chatter',
+      messageType: 'std_msgs/String'
+    });
+
+    chatterSub.subscribe(this.chatterCallback);
+  }
+
+  // ROS Callback Functions
+  chatterCallback(message: any) {
+    this.setState({
+      value: message.data
+    });
+  }
+  
   render() {
-    return <h1>Sensors page!</h1>
+    return <h1>Received message: {this.state.value}</h1>
   }
 }
